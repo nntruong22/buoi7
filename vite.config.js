5182cc7f1789a838882082c18a2e0e39dac3efbc
nntruong22 <nnt221226@gmail.com>
2026-02-02 11:00:02 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Buoi7/',
+  base: '/buoi7/',
 })
 
 console.log("Vite config loaded");
